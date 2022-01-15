542dbdce5ef86f28c2ae229ca94e43699712b016
Lorenzo Murarotto <lnzmrr@gmail.com>
2022-01-15 16:37:27 +0100

    initial commit

---
+++ firefox-deflector.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+#
+# Description:  Proxy for the firefox executable that uses
+#               the currently running profile.
+#
+# Usage:        firefox-deflector.sh [URI]
+
+set -Eeu
+
+FIREFOX_EXE="/usr/lib/firefox/firefox"
+
+RUNNING_INSTANCES="$(
+    ps -F -u $UID |
+        tr -s " " |
+        cut -d " " -f 11- |
+        grep -P "^$FIREFOX_EXE" |
+        grep -Pv '\-childID \d+|\-contentproc' ||
+        :
+)"
+
+RUNNING_PROFILE_NAME="$(
+    echo "$RUNNING_INSTANCES" |
+        grep -Po '(?<=\-P)\s+\w+\s+' |
+        head -n 1 |
+        awk '{$1=$1};1' ||
+        :
+)"
+
+RUNNING_PROFILE_PATH="$(
+    echo "$RUNNING_INSTANCES" |
+        grep -Po '(?<=\-profile) (\/[^\/|\s]+)+' |
+        head -n 1 |
+        awk '{$1=$1};1' ||
+        :
+)"
+
+echo "$RUNNING_INSTANCES"
+
+if [ -n "$RUNNING_PROFILE_NAME" ]; then
+    $FIREFOX_EXE -P "$RUNNING_PROFILE_NAME" $@
+elif [ -n "$RUNNING_PROFILE_PATH" ]; then
+    $FIREFOX_EXE -profile "$RUNNING_PROFILE_PATH" $@
+else
+    $FIREFOX_EXE $@
+fi
---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+#
+# Description:  Install the firefox-deflector utility in the current system.
+#
+
+set -Eeu
+
+INSTALL_LOCATION="$HOME/.local/bin"
+SHORTCUT_LOCATION="$HOME/.local/share/applications"
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+
+if ! [ -x "$(command -v firefox)" ]; then
+    echo 'Error: firefox should be installed and in path.' >&2
+    exit 1
+fi
+
+mkdir -p "$INSTALL_LOCATION" "$SHORTCUT_LOCATION"
+
+ln -sf "$SCRIPT_DIR/firefox-deflector.sh" "$INSTALL_LOCATION/firefox-deflector"
+ln -sf "$SCRIPT_DIR/firefox-deflector.desktop" "$SHORTCUT_LOCATION/firefox-deflector.desktop"
