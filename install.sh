cabde08605be6bdae8826e55e28c411387f679fe
Lorenzo Murarotto <lnzmrr@gmail.com>
2022-01-16 09:58:04 +0100

    check for non standard install locations

--- install.sh
+++ install.sh
@@ -5,12 +5,19 @@
 
 set -Eeu
 
+FIREFOX_EXE="/usr/lib/firefox/firefox"
 INSTALL_LOCATION="$HOME/.local/bin"
 SHORTCUT_LOCATION="$HOME/.local/share/applications"
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 
 if ! [ -x "$(command -v firefox)" ]; then
     echo 'Error: firefox should be installed and in path.' >&2
+    exit 1
+fi
+
+if ! [ -x "$FIREFOX_EXE" ]; then
+    echo "Error: this tool expects firefox to be installed at $FIREFOX_EXE" >&2
+    echo "Please file an issue at https://github.com/murar8/firefox-deflector/issues to receive support."
     exit 1
 fi
 
