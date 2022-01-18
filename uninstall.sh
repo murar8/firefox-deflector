dbc4053bd826e4fc779aa1e92fbcfaf1657599cc
Lorenzo Murarotto <lnzmrr@gmail.com>
2022-01-18 20:29:16 +0100

    add uninstall script

---
+++ uninstall.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+#
+# Description:  Remove the firefox-deflector utility from the current system.
+#
+
+set -Eeu
+
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+
+rm -rf $HOME/.local/bin/firefox-deflector $HOME/.local/share/applications/firefox-deflector.desktop $SCRIPT_DIR
